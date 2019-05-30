4a2c7b05854815a8e65ab71794c3c0461dc8bd09
jake lam <jlam2017@gmail.com>
2019-05-30 16:23:11 -0500

    FINAL COMMIT
    THE PROJECT IS NOW FINISHED

---
+++ Assets/Prefabs/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ScreenshotHandler.TakeScreenshot_static(1900, 1600);
+        }
+    }
+
+    public GameObject completeLeveUI;
+
+    public void CompleteLevel() 
+    {
+        completeLeveUI.SetActive(true);
+    }
+
+    public void LoadNextLevel() 
+    {
+       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Endgame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endgame : MonoBehaviour {
+
+    public GameManager manager;
+
+    public void OnTriggerEnter()
+    {
+        Debug.Log("You won!!");
+        manager.CompleteLevel();
+    }
+}
